df233caef6001447d368a4f2a43bc4bf8b3018c8
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-22 15:26:44 +0530

    added register page route in app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register.jsx';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/api/auth/login' element={<Login />} />
+        <Route path='/api/auth/register' element={<Register />} />
       </Routes>
     </div>
     </Router>
