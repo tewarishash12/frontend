2de59e9f0d3d27ce1f430dcfbff2f618c70b3d20
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-22 15:26:58 +0530

    added register link to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,6 +18,9 @@ export default function Navbar() {
                             <li className="nav-item">
                                 <Link className="nav-link" to="/api/auth/login">Login</Link>
                             </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/api/auth/register">Register</Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
