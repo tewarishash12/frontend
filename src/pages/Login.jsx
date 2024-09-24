e1ca7616cb840d1b27b4da190649e5b8b1c523b1
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-24 22:02:26 +0530

    Added token storage in localStorage in the frontend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const [credentials, setCredentials] = useState({ email: "", password: "" })
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // synthetic event
+    const res = await fetch(`http://localhost:5000/api/auth/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
+    });
+
+    const json = await res.json();
+    console.log(json);
+
+    if (!json.success)
+      alert("Enter valid credentials");
+
+    if(json.success){
+      localStorage.setItem("authToken", json.authToken);
+      console.log(localStorage.getItem("authToken"));
+      navigate('/');
+    }
+  }
+
+  const change = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  }
+
   return (
-    <div>Login</div>
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" name='email' value={credentials.email} onChange={change} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" name='password' value={credentials.password} onChange={change} />
+        </div>
+        <button type="submit" className="btn btn-info">Submit</button>
+        <Link to="/api/auth/register" className="m-3 btn btn-danger">Don't have an Account?</Link>
+
+      </form>
+    </div>
   )
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Register() {
+
+    const [credentials, setCredentials] = useState({username:"",email:"",password:"",location:""})
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault(); // synthetic event
+        const res = await fetch(`http://localhost:5000/api/auth/register`, {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({username:credentials.username, email:credentials.email, password:credentials.password, location:credentials.location})
+        });
+
+        const json = await res.json();
+        console.log(json);
+
+        if(!json.success)
+            alert("Enter valid credentials");
+    }
+
+    const change = (e) =>{
+        setCredentials({...credentials,[e.target.name]:e.target.value});
+    }
+
+    return (
+        <div className='container'>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label htmlFor="username" className="form-label">UserName</label>
+                    <input type="text" className="form-control" name='username' value={credentials.username} onChange={change} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control" name='email' value={credentials.email} onChange={change} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" name='password' value={credentials.password} onChange={change} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="location" className="form-label">Location</label>
+                    <input type="text" className="form-control" name='location' value={credentials.location} onChange={change} />
+                </div>
+                <button type="submit" className="btn btn-info">Submit</button>
+                <Link to="/api/auth/login" className="m-3 btn btn-danger">Already a user?</Link>
+            </form>
+        </div>
+
+    )
+}
