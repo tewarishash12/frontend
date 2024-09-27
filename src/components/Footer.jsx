154d33b1c5d562103435ba1e8678bb3dc88664c5
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-27 18:59:50 +0530

    Changed the website name (didn't like the new one)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
         
       </Link>
-      <span className="text-muted">© 2024 HungerSoltion, Inc</span>
+      <span className="text-muted">© 2024 BiteDash, Inc</span>
     </div>
 
     <ul className="nav col-md-4 justify-content-end list-unstyled d-flex"></ul>
