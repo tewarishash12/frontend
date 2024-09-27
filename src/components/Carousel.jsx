99092d508b34bb4a47bee4d46c9b4ec232453ae7
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-27 09:23:26 +0530

    Added more images in carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import burger from "../assets/carousel/burger.jpg";
 import pizza from "../assets/carousel/pizza.jpeg";
 import momos from "../assets/carousel/momos.jpg";
+import biryani from "../assets/carousel/biryani.jpeg"
+import tikka from "../assets/carousel/panner-tikka.jpeg"
 
 export default function Carousel() {
     return (
         <div>
             <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel" data-bs-wrap="true" style={{ objectFit: "contain !important" }}>
                 <div className="carousel-inner" style={{ maxHeight: "550px" }}>
-                    
-                    {/* Search bar overlay */}
                     <div className="carousel-caption" style={{ zIndex: "10" }}>
                         <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
@@ -19,15 +19,23 @@ export default function Carousel() {
 
                     <div className="carousel-item active">
                         <img src={burger} className="d-block w-100" alt="burger"
-                            style={{ maxHeight: "550px", objectFit: "cover", filter: "brightness(40%)" }} />
+                        style={{ maxHeight: "550px", objectFit: "cover", filter: "brightness(40%)" }} />
                     </div>
                     <div className="carousel-item">
                         <img src={pizza} className="d-block w-100" alt="pizza"
-                            style={{ maxHeight: "550px", objectFit: "cover", filter: "brightness(40%)" }} />
+                        style={{ maxHeight: "550px", objectFit: "cover", filter: "brightness(40%)" }} />
                     </div>
                     <div className="carousel-item">
                         <img src={momos} className="d-block w-100" alt="momos"
-                            style={{ maxHeight: "550px", objectFit: "cover", filter: "brightness(40%)" }} />
+                        style={{ maxHeight: "550px", objectFit: "cover", filter: "brightness(40%)" }} />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={biryani} className="d-block w-100" alt="momos"
+                        style={{ maxHeight: "550px", objectFit: "cover", filter: "brightness(40%)" }} />
+                    </div>
+                    <div className="carousel-item">
+                        <img src={tikka} className="d-block w-100" alt="momos"
+                        style={{ maxHeight: "550px", objectFit: "cover", filter: "brightness(40%)" }} />
                     </div>
                 </div>
 
