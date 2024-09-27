167c6a8f74dce3fc183958545add441cb8438727
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-27 23:05:00 +0530

    Wrapped the App.js with CartProvider functionality

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import Register from './pages/Register.jsx';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import { CartProvider } from './components/ContextReducer.jsx';
 
 function App() {
   return (
-    <Router>
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/api/auth/login' element={<Login />} />
-        <Route path='/api/auth/register' element={<Register />} />
-      </Routes>
-    </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/api/auth/login' element={<Login />} />
+            <Route path='/api/auth/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
