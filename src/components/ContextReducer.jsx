88f6751e18552ce8d0869ac46f9965a296d7ff5b
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-28 22:36:59 +0530

    Added update and remove functionalities for the cart page

--- src/components/ContextReducer.jsx
+++ src/components/ContextReducer.jsx
@@ -7,7 +7,17 @@ const reducer = (state,action) =>{
     switch(action.type){
         case "Add":
             return [...state, {id: action.id, name: action.name, quan: action.quan, size: action.size, price: action.price, img: action.img}];
-        
+        case "Remove":
+            let arr = [...state];
+            arr.splice(action.index, 1)
+            return arr;
+        case "Update":
+            let list = [...state];
+            list.find((food,index)=>{
+                if(food.id === action.id)
+                    list[index] = {...food, quan: parseInt(action.quan)+parseInt(food.quan), price:action.price+food.price}
+            })
+            return list;    
         default:
             console.log("Error in reducer")
     }
