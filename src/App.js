30806505edabe5f98f775f99e83ccea5495bcfff
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-29 23:16:30 +0530

    Added login based routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import './App.css';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register.jsx';
+import MyOrders from './pages/MyOrders.jsx';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import { CartProvider } from './components/ContextReducer.jsx';
-import MyOrders from './pages/MyOrders.jsx';
 
 function App() {
+  const email = localStorage.getItem("authToken");
   return (
     <CartProvider>
       <Router>
         <div>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/api/auth/login' element={<Login />} />
-            <Route path='/api/auth/register' element={<Register />} />
-            <Route path='/api/orders/orderhistory' element={<MyOrders />} />
+            {email ? (
+              <>
+                <Route path='/' element={<Home />} />
+                <Route path='/api/orders/orderhistory' element={<MyOrders />} />
+                <Route path='*' element={<Navigate to="/" />} />
+              </>
+            )
+            : (
+              <>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/api/auth/login' element={<Login />} />
+                  <Route path='/api/auth/register' element={<Register />} />
+                  <Route path='*' element={<Navigate to="/api/auth/login" />} />
+                </>
+              )
+            }
           </Routes>
         </div>
       </Router>
