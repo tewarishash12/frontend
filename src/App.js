577918abdb0a453c1b19714c13b6e6a40993d8f3
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-29 22:59:03 +0530

    Added routes for orderhistory page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import { CartProvider } from './components/ContextReducer.jsx';
+import MyOrders from './pages/MyOrders.jsx';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/api/auth/login' element={<Login />} />
             <Route path='/api/auth/register' element={<Register />} />
+            <Route path='/api/orders/orderhistory' element={<MyOrders />} />
           </Routes>
         </div>
       </Router>
