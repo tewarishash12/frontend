c3f549ea25b13bad59c6bde26820dc8d2e20e8ba
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-29 23:17:36 +0530

    Added navigation route after logging in

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Register() {
+    const navigate = useNavigate();
 
     const [credentials, setCredentials] = useState({username:"",email:"",password:"",location:""})
 
@@ -20,6 +21,8 @@ export default function Register() {
 
         if(!json.success)
             alert("Enter valid credentials");
+        else
+            navigate('/api/auth/login');
     }
 
     const change = (e) =>{
