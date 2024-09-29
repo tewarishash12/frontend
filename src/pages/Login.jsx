91ef3fcfea3415df6b11e251cf098802cad2b20c
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-29 12:18:59 +0530

    Created another token to store the email of the logged in user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,8 +21,8 @@ export default function Login() {
       alert("Enter valid credentials");
 
     if(json.success){
+      localStorage.setItem("userEmail", credentials.email);
       localStorage.setItem("authToken", json.authToken);
-      console.log(localStorage.getItem("authToken"));
       navigate('/');
     }
   }
